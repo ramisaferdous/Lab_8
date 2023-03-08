8cf7c0c0d8f0b664792d6ca9efb32c833b99fb7e
ramisaferdous <rifahramisa@iut-dhaka.edu>
2023-03-08 23:02:00 +0600

    Added new file

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMS
+{
+    public partial class Form1 : Form
+    {
+        List<user> users = new List<user>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBox13.Text);
+            string name = textBox14.Text;
+            string Addres = textBox15.Text;
+            user p1 = new user(id, name, Addres);
+            users.Add(p1);
+            textBox13.Clear();
+            textBox14.Clear();
+            textBox15.Clear();
+            MessageBox.Show("USER CREATED SUCCESSFULLY");
+        }
+    }
+}
---
+++ user.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS
+{
+    internal class user {
+        List<user> users = new List<user>();
+        public int id;
+        public string name;
+        public string address;
+        public user(int _id,string _name,string _address)
+        {
+            id = _id;
+            name = _name;
+            address = _address;
+
+        }
+        public void add_user(user b)
+        {
+            users.Add(b);
+        }
+       
+    
+    }
+}
